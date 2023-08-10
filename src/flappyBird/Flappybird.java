63f16a649102bd857e84d069189b651d919ad735
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2023-08-10 13:54:09 +0600

    Almost done adding the base.

--- src/flappyBird/Flappybird.java
+++ src/flappyBird/Flappybird.java
@@ -26,12 +26,15 @@ public class Flappybird implements ActionListener
         rand = new Random();
 
         jframe.add(renderer);
+        jframe.setTitle("Flappy Bird");
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.setSize(WIDTH, HEIGHT);
         jframe.setResizable(false);
         jframe.setVisible(true);
 
         bird = new Rectangle(WIDTH / 2 - 10, HEIGHT / 2 - 10, 20, 20);
+        columns = new ArrayList<Rectangle>();
+        rand = new Random();
 
         timer.start();
     }
@@ -41,6 +44,29 @@ public class Flappybird implements ActionListener
         g.setColor(Color.cyan);
         g.fillRect(0, 0, WIDTH, HEIGHT);
 
+        g.setColor(Color.red);
+        g.fillRect(bird.x, bird.y, bird.width, bird.height);
+
+        g.setColor(Color.orange);
+        g.fillRect(0, HEIGHT -120, WIDTH,150);
+
+        g.setColor(Color.green);
+        g.fillRect(0, HEIGHT -120, WIDTH,20);
+    }
+
+    public void addColumn()
+    {
+        int space = 300;
+        int width = 100;
+        int heigth = 50 + rand.nextInt(300);
+
+        columns.add(new Rectangle())
+    }
+
+    public void paintColumn(Graphics g, Rectangle column)
+    {
+        g.setColor(Color.green);
+        g.fillRect(column.x, column.y, column.width, column.height);
     }
 
     @Override
